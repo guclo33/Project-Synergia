7138d74b27c75f7e86aae889fd5b23111c3eae7d
guclo33 <guillaume_cloutier123@hotmail.com>
2024-09-23 19:45:17 -0400

    prompt v1.0 et moyennes archetype et couleurs

--- test fonctionbasedonnée.py
+++ test fonctionbasedonnée.py
@@ -1,39 +1,76 @@
 import pandas as pd
 
-nom = "Maxime, Pépin"
+nom = "Veillette, Joanie"
 
 #DONNÉES EXCEL
 
 #Pour le client
-synergia = pd.read_excel("C:/Users/Guillaume Cloutier/OneDrive/Synergia/Synergia.xlsx", sheet_name="Réponses 3")
+synergia = pd.read_excel("C:/Users/Guillaume Cloutier/OneDrive/Synergia/Synergia.xlsx", sheet_name="synergia_mlm")
+
+synergia_model = pd.read_excel("C:/Users/Guillaume Cloutier/OneDrive/Synergia/Synergia.xlsx", sheet_name="Réponses 3")
 
 synergia_nom = pd.DataFrame(synergia.loc[synergia["Nom"]== nom])
 
+
+#plage de questions utilisé pour les prompts
+
 plage_nom = synergia_nom.iloc[:,2:3]
-plage_questions1_11 = synergia_nom.iloc[:,6:50]
-plage_questions17_20 = synergia_nom.iloc[:,62:78]
-plage_questions1_20 = synergia_nom.iloc[:,6:78]
-plage_questions12_14 = synergia_nom.iloc[:,50:62]
-plage_questions15_16 = synergia_nom.iloc[:,106:114]
-plage_questions_dev1 = synergia_nom.iloc[:,104:106]
-plage_questions_dev2 = synergia_nom.iloc[:,115:116]
-plage_questions_complet = synergia_nom.iloc[:,6:115]
-plage_questions21_26 = synergia_nom.iloc[:,78:102]
+plage_questions1_15 = synergia_nom.iloc[:,6:66]
+plage_questions16_24 = synergia_nom.iloc[:,66:102]
+plage_questions_developpement = synergia_nom.iloc[:,102:105]
+plage_questionnaire_complet = synergia_nom.iloc[:,6:105]
 
 
+#definition de la fonction pour formatter les résultats des bases de données
 def excel_to_string(*args):
   return pd.concat(args, axis=1).transpose().to_string(header=False)
 
 
 
-#section 1
-synergia_section1= pd.concat([plage_nom, plage_questions1_11, plage_questions17_20], axis=1)
+#section couleurs
 
-synergia_section1_transposed = synergia_section1.transpose()
-
-synergia_section1_string = synergia_section1_transposed.to_string(header=False)
+synergia_couleur_string = excel_to_string(plage_nom, plage_questions1_15)
 
 
-print(synergia_section1_string)
+#section archétype
 
-print(excel_to_string(plage_nom, plage_questions1_11, plage_questions17_20))
+synergia_archetype_string = excel_to_string(plage_nom, plage_questions16_24)
+
+
+#Questions développement
+
+
+synergia_section_developpement_string = excel_to_string(plage_nom, plage_questions_developpement)
+
+
+#Questionnaire complet
+
+#synergia_complet_string = excel_to_string(plage_nom, plage_questionnaire_complet)
+
+synergia_complet_string = synergia_couleur_string + "\n" +synergia_archetype_string+ "\n" + synergia_section_developpement_string
+
+#Pour le model 1
+
+synergia_model1 = synergia_model.iloc[[92]]
+
+plage_model1 = synergia_model1.iloc[:,2]
+plage_model1_questions1_11 = synergia_model1.iloc[:,6:50]
+plage_model1_questions17_20 = synergia_model1.iloc[:,62:78]
+
+synergia_model1_section1_string = excel_to_string(plage_model1, plage_model1_questions1_11, plage_model1_questions17_20)
+
+
+
+#Pour le model 2
+
+synergia_model2 = synergia_model.iloc[[88]]
+
+plage_model2 = synergia_model2.iloc[:,2]
+plage_model2_questions1_11 = synergia_model2.iloc[:,6:50]
+plage_model2_questions17_20 = synergia_model2.iloc[:,62:78]
+
+synergia_model2_section1_string = excel_to_string(plage_model2, plage_model2_questions1_11, plage_model2_questions17_20)
+
+
+print(synergia_model2_section1_string)
+
